105b324f66cde1efe8fa7f3458655b036fde5691
Thomas Morrill <morrill.math@gmail.com>
2022-07-08 22:55:08 -0400

    bugfix; tidy up

--- rewrite.py
+++ rewrite.py
@@ -4,13 +4,17 @@ from pyo import Sine, Server
 
 
 def muse(out_a, out_b, out_c, out_d, feed_w, feed_x, feed_y, feed_z):
-    """Generate output of the Triadex Muse."""
+    """Emulate the Triadex Muse."""
+    if (
+        min(out_a, out_b, out_c, out_d, feed_w, feed_x, feed_y, feed_z) < 0
+        or max(out_a, out_b, out_c, out_d, feed_w, feed_x, feed_y, feed_z) > 40
+    ):
+        raise ValueError("tap locations must be integers from 0 to 40.")
+
     shift_register = [randint(0, 1) for _ in range(31)]
     t = 0
 
     while True:
-        t += 1
-        t %= 32
 
         constant_taps = [0, 1]
         bit_taps = [(t >> i) & 1 for i in range(5)]
@@ -26,23 +30,24 @@ def muse(out_a, out_b, out_c, out_d, feed_w, feed_x, feed_y, feed_z):
 
         semitones = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24, 24]
 
+        print(all_taps)
         yield semitones[scale_degree]
 
         bit = (
-            all_taps[feed_w]
-            + all_taps[feed_x]
-            + all_taps[feed_y]
-            + all_taps[feed_z]
+            all_taps[feed_w] + all_taps[feed_x]
+            + all_taps[feed_y] + all_taps[feed_z]
         ) % 2
 
         shift_register.insert(0, bit)
         shift_register.pop()
 
+        t += 1
+
 
 if __name__ == "__main__":
-    semitone = muse(out_a=17, out_b=17, out_c=18, out_d=19, feed_w=4, feed_x=19, feed_y=8, feed_z=5)
+    semitone = muse(17, 17, 18, 19, 4, 19, 8, 25)
 
-    root_hz = 261.6
+    C_hz = 261.6
     half_step_ratio = 1.05946882217
     bpm = 240
     seconds = 60.0 / bpm
@@ -52,7 +57,6 @@ if __name__ == "__main__":
 
     while True:
         steps = next(semitone)
-        print(steps)
-        freq = root_hz * half_step_ratio ** steps
+        freq = C_hz * half_step_ratio**steps
         note = Sine(freq=freq, mul=0.05).out()
         time.sleep(seconds)
